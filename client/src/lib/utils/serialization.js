a8236921e4bb757191f3f68664069f75256734eb
Carlos Gonzalez <justdevelopitmx@gmail.com>
2025-05-28 15:55:00 +0700

    Deleting old client

--- client/src/lib/utils/serialization.js
+++
@@ -1,8 +0,0 @@
-/**
- * Serializes non-POJOs
- * @param {any} obj - The object to serialize
- * @returns {any}
- */
-export const serializeNonPOJOs = (obj) => {
-	return structuredClone(obj);
-};
--- client/src/routes/(auth)/+layout.server.js
+++
@@ -1,8 +0,0 @@
-import { redirect } from '@sveltejs/kit';
-
-export const load = async ({ locals }) => {
-	// @ts-ignore
-	if (!locals.user) {
-		redirect(303, '/login');
-	}
-};
